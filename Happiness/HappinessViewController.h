082b51d975241c73be9640973e274f00ecd2da9b
Frank Shen <shenjunru1992@gmail.com>
2012-06-29 22:14:47 +0800

    first commit

---
+++ Happiness/HappinessViewController.h
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.h
+//  Happiness
+//
+//  Created by 沈 君儒 on 12-6-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HappinessViewController : UIViewController
+
+@end
